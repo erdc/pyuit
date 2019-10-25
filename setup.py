171b0c75d8bff84fc1678ebad16cb0424810c018
rditlsc9 <Scott.D.Christensen@erdc.dren.mil>
2019-10-25 10:59:53 -0500

    panel_util to gui_tools

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup
 
 install_requires = [
-    'flask', 'PyYAML', 'requests',
+    'flask', 'PyYAML', 'requests', 'dodcerts',
 ]
 
 extras_require = dict()
 
-extras_require['recommended'] = ['dodcerts']
+extras_require['guitools'] = ['panel', 'param', 'holoviews', 'pandas']
 
-extras_require['examples'] = (['jupyterlab', 'nodejs', 'panel', 'param', 'holoviews', 'pandas'])
+extras_require['examples'] = ['jupyterlab', 'nodejs'] + extras_require['guitools']
 
-extras_require['tests'] = (['pytest', 'flake8'])
+extras_require['tests'] = ['pytest', 'flake8']
 
 setup(
     name='pyuit',
-    version='0.3.0a14',
+    version='0.3.0a15',
     description="Python wrapper for DoD HPCMP UIT+ rest interface",
     author="Scott Christensen",
     author_email='scott.d.christensen@erdc.dren.mil',
     url='https://github.com/erdc/uit',
-    packages=['uit'],
+    packages=['uit', 'uit.gui_tools'],
     entry_points={
         'console_scripts': [
             'uit=uit.cli:cli'
