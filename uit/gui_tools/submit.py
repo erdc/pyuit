278ae29accf89402e73127eb28017d3de43b09f4
Shyla Clark <sclark@geocent.com>
2020-08-27 19:45:32 -0500

    Added LoadingSpinner using js_on_click() that renders when the user clicks the Validate or Submit buttons.

--- uit/gui_tools/submit.py
+++ uit/gui_tools/submit.py
@@ -1,6 +1,5 @@
 import param
 import panel as pn
-from functools import partial
 
 from .file_browser import HpcFileBrowser
 from .configurable import HpcConfigurable
@@ -17,6 +16,7 @@ class PbsScriptInputs(param.Parameterized):
     wall_time = param.String(default='00:05:00', precedence=6)
     queue = param.ObjectSelector(default=QUEUES[0], objects=QUEUES, precedence=7)
     submit_script_filename = param.String(default='run.pbs', precedence=8)
+    gui = param.ClassSelector(pn.Column, default=pn.Column())
 
     def update_hpc_connection_dependent_defaults(self):
         if not self.uit_client.connected:
@@ -51,8 +51,8 @@ class PbsScriptInputs(param.Parameterized):
 class PbsScriptAdvancedInputs(HpcConfigurable):
     env_names = param.List()
     env_values = param.List()
-    env_browser = param.List()
-    browse_view = param.ClassSelector(pn.Row, default=pn.Row())
+    browse = param.Action(lambda self: self.toggle_file_browser(), label='📂')
+    browse_toggle = param.Boolean(default=False)
 
     def update_environ(self, *events):
         for event in events:
@@ -76,42 +76,28 @@ class PbsScriptAdvancedInputs(HpcConfigurable):
         widget.param.watch(self.update_environ, ['value'], onlychanged=True)
         return widget
 
-    @param.depends()
-    def env_file_browser_widget(self, tag, **kwargs):
-        widget = pn.widgets.Button(name='📂', css_classes=[tag], width=30, align='end', **kwargs)
-        widget.on_click(self.toggle_file_browser)
-        return widget
+    def toggle_file_browser(self):
+        self.browse_toggle = not self.browse_toggle
 
-    def toggle_file_browser(self, *events):
-        for event in events:
-            _, is_key, i = event.obj.css_classes[0].split('_')
-            i = int(i)
-            new_val = self.env_values[i]
-            new_browser_pn = HpcFileBrowser(self.uit_client)
-            new_browser_pn.callback = partial(self.new_callback, a=new_val, b=new_browser_pn)
-            self.browse_view[:] = [new_browser_pn.panel]
-
-    @staticmethod
-    def new_callback(new_selection, a, b):
-        a.value = b.value[0]
+    @param.depends('browse_toggle')
+    def file_browser_view(self):
+        if self.browse_toggle:
+            return HpcFileBrowser(self.uit_client).panel
+        else:
+            return pn.layout.Spacer()
 
     @param.depends('environment_variables')
     def environment_variables_view(self):
         self.env_names = list()
         self.env_values = list()
-        self.env_browser = list()
-
         for i, (k, v) in enumerate(self.environment_variables.items()):
             name_widget = self.env_var_widget(val=k, tag=f'env_key_{i}')
             val_widget = self.env_var_widget(val=str(v), tag=f'env_val_{i}')
-            browser_widget = self.env_file_browser_widget(tag=f'env_browser_{i}')
             self.env_names.append(name_widget)
             self.env_values.append(val_widget)
-            self.env_browser.append(browser_widget)
 
         self.env_names.append(self.env_var_widget(val=None, tag='env_key_-1', placeholder='NEW_ENV_VAR'))
         self.env_values.append(self.env_var_widget(val=None, tag='env_val_-1', disabled=True))
-        self.env_browser.append(self.env_file_browser_widget(tag='env_browser_-1', disabled=True))
 
         self.env_names[0].name = 'Name'
         self.env_values[0].name = 'Value'
@@ -119,10 +105,13 @@ class PbsScriptAdvancedInputs(HpcConfigurable):
         return pn.Column(
             '<h3>Environment Variables</h3>',
             pn.Column(
-                *[pn.Row(k, v, b, width_policy='max') for k, v, b in
-                  zip(self.env_names, self.env_values, self.env_browser)],
+                *[pn.Row(k, v, width_policy='max') for k, v in zip(self.env_names, self.env_values)],
             ),
-            self.browse_view,
+            pn.Row(
+                '<h4>File Browser</h4>',
+                pn.Param(self, parameters=['browse'], widgets={'browse': {'width': 30}}, show_name=False),
+            ),
+            self.file_browser_view,
         )
 
     def advanced_options_view(self):
@@ -228,10 +217,14 @@ class HpcSubmit(PbsScriptInputs, PbsScriptAdvancedInputs):
             button = 'validate_btn'
             button_type = 'primary'
 
-        return pn.Param(
+        spn = pn.widgets.indicators.LoadingSpinner(value=True, color='primary', aspect_ratio=1, width=0)
+        action_btn = pn.Param(
             self.param[button],
             widgets={button: {'button_type': button_type, 'width': 200}}
-        )
+        )[0]
+        action_btn.js_on_click(args={'btn': action_btn, 'spn': spn}, code='btn.visible=false; spn.width=50;')
+        self.gui[:] = [action_btn, spn]
+        return self.gui
 
     def submit_view(self):
         return pn.Column(
