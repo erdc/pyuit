2a52f54683af5d7b299b89f64c8117ba0122d974
Shyla Clark <shyla.clark@geocent.com>
2020-09-22 09:31:51 -0500

    Making sure my update to connect.py is pushed to my fork

--- uit/gui_tools/connect.py
+++ uit/gui_tools/connect.py
@@ -79,7 +79,7 @@ class HpcConnect(param.Parameterized):
 
     @param.depends('system', watch=True)
     def update_node_options(self):
-        if self.uit_client.login_nodes:
+        if self.uit_client.login_nodes: # Required after latest updates to master branch
             options = self.uit_client.login_nodes[self.system]
             self.param.exclude_nodes.objects = options
             options = options.copy()
