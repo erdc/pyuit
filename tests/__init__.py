4f1fca9aa97c89f2fe4b143af5c58f8a2cd22906
tcoumbe2016 <67707824+tcoumbe2016@users.noreply.github.com>
2022-11-07 09:53:00 -0600

    Unit test uit a (#36)
    * Getting current Unit Tests set up, so they do not fail. This is phase 1 A.
    
    * Getting current Unit Tests set up, so they do not fail.
    
    * Completion of test_pbs_script.py tests and PEP8 Corrections.
    
    * Start of UnitTests for uit.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,3 @@
 from tests.test_pbs_script import TestPBSScript  # noqa: F401
+from .test_cli import test_cli_template  # noqa: F401
+from .test_uit import TestUIT  # noqa: F401
--- tests/test_uit.py
+++ tests/test_uit.py
@@ -1,52 +1,133 @@
 import unittest
 from unittest import mock
-
+from pathlib import PurePosixPath
 from uit.uit import Client
 
 
 class TestUIT(unittest.TestCase):
 
     def setUp(self):
-        pass
+        from uit.uit import Client
+        with mock.patch('uit.uit.Client.get_userinfo') as _:
+            self.client = Client(token='test_token')
+            self.client.connected = True
 
     def tearDown(self):
         pass
 
-    def test_client(self):
-        client = Client(token='test_token')
-        self.assertEqual('test_token', client.token)
+    def test_optional_dependency_has_pandas(self):
+        from uit import uit as uit_test
+        from importlib import reload
+        import builtins
+        real_import = builtins.__import__
+        try:
+            def mock_import(name, *args):
+                if name == 'pandas':
+                    raise ModuleNotFoundError
+                else:
+                    return real_import(name, *args)
 
-    def test_load_token(self):
-        client = Client(token='test_token')
-        # call the method
-        self.assertEqual('test_token', client.load_token())
+            builtins.__import__ = mock_import
+            reload(uit_test)
+        finally:
+            builtins.__import__ = real_import
+        self.assertEqual(uit_test.has_pandas, False)
+
+    @mock.patch('uit.uit.open')
+    @mock.patch('uit.uit.yaml.safe_load')
+    def test_init_no_token(self, mock_yaml, _):
+        mock_yaml.return_value = {
+            'client_id': 'client_id',
+            'client_secret': 'client_secret'
+        }
+        Client()
+
+    @mock.patch('uit.uit.open')
+    @mock.patch('uit.uit.yaml.safe_load')
+    def test_init_no_credentials(self, mock_yaml, _):
+        mock_yaml.return_value = {}
+        self.assertRaises(ValueError, Client)
+
+    def test_ensure_connected(self):
+        self.client.connected = False
+        self.assertRaises(
+            RuntimeError, self.client.call, 'cmd'
+        )
+
+    @mock.patch('uit.uit.Client')
+    def test_HOME(self, _):
+        self.client.env.HOME = 'HOME env'
+        res = self.client.HOME
+        self.assertEqual(PurePosixPath('HOME env'), res)
+
+    @mock.patch('uit.uit.Client')
+    def test_WORKDIR(self, _):
+        self.client.env.WORKDIR = 'WORKDIR'
+        res = self.client.WORKDIR
+        self.assertEqual(PurePosixPath('WORKDIR'), res)
+
+    @mock.patch('uit.uit.Client')
+    def test_WORKDIR2(self, _):
+        self.client.env.WORKDIR2 = 'WORKDIR2'
+        res = self.client.WORKDIR2
+        self.assertEqual(PurePosixPath('WORKDIR2'), res)
+
+    @mock.patch('uit.uit.Client')
+    def test_CENTER(self, _):
+        self.client.env.CENTER = 'CENTER'
+        res = self.client.CENTER
+        self.assertEqual(PurePosixPath('CENTER'), res)
+
+    def test_token(self):
+        self.assertEqual('test_token', self.client.token)
+
+    def test_login_node(self):
+        self.assertEqual(None, self.client.login_node)
+
+    def test_login_nodes(self):
+        self.assertEqual(None, self.client.login_nodes)
+
+    def test_system(self):
+        self.assertEqual(None, self.client.system)
+
+    def test_systems(self):
+        self.assertEqual(None, self.client.systems)
+
+    def test_uit_url(self):
+        self.assertEqual(None, self.client.uit_url)
+
+    def test_uit_urls(self):
+        self.assertEqual(None, self.client.uit_urls)
+
+    def test_user(self):
+        self.assertEqual(None, self.client.user)
+
+    def test_userinfo(self):
+        self.assertEqual(None, self.client.userinfo)
+
+    def test_username(self):
+        self.assertEqual(None, self.client.username)
 
     @mock.patch('uit.uit.Client.call')
     @mock.patch('uit.uit.Client.put_file')
     def test_submit(self, _, mock_call):
-        client = Client(token='test_token')
-        client.connected = True
         mock_call.return_value = 'J001'
 
-        ret = client.submit(pbs_script='test_script.sh', working_dir='\\test\\workdir')
+        ret = self.client.submit(pbs_script='test_script.sh', working_dir='\\test\\workdir')
 
         self.assertEqual('J001', ret)
 
     @mock.patch('uit.uit.Client.call')
     @mock.patch('uit.uit.Client.put_file')
     def test_submit_runtime_error(self, mock_put_file, mock_call):
-        client = Client(token='test_token')
-        client.connected = True
         mock_put_file.return_value = {'success': 'false', 'error': 'test_error'}
         mock_call.return_value = 'J001'
 
-        self.assertRaises(RuntimeError, client.submit, pbs_script='test_script.sh', working_dir='\\test\\workdir')
+        self.assertRaises(RuntimeError, self.client.submit, pbs_script='test_script.sh', working_dir='\\test\\workdir')
 
     @mock.patch('uit.uit.Client.call')
     @mock.patch('uit.uit.Client.put_file')
     def test_submit_call_error(self, _, mock_call):
-        client = Client(token='test_token')
-        client.connected = True
         mock_call.side_effect = RuntimeError
 
-        self.assertRaises(RuntimeError, client.submit, pbs_script='test_script.sh', working_dir='\\test\\workdir')
+        self.assertRaises(RuntimeError, self.client.submit, pbs_script='test_script.sh', working_dir='\\test\\workdir')
