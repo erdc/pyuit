3423cfe0955a187436b60929fec3668717ef307f
Mark Lugar <14322382+araglu@users.noreply.github.com>
2024-04-22 15:52:43 -0500

    Fix under-indentation from renaming pbs_gpu

--- tests/test_pbs_script.py
+++ tests/test_pbs_script.py
@@ -147,7 +147,7 @@ class TestPBSScript(unittest.TestCase):
 
     def test_get_render_required_directives_block_for_narwhal_compute_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=1, max_time="20:30:30",
-                            node_type='compute', system='narwhal')
+                               node_type='compute', system='narwhal')
         res = pbs_script.render_required_directives_block()
         self.assertIn('#PBS -N ' + pbs_script.name, res)
         self.assertIn('#PBS -A ' + pbs_script.project_id, res)
@@ -158,27 +158,27 @@ class TestPBSScript(unittest.TestCase):
 
     def test_get_render_required_directives_block_for_narwhal_gpu_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=1, max_time="20:30:30",
-                            node_type='gpu', system='narwhal')
+                               node_type='gpu', system='narwhal')
         res = pbs_script.render_required_directives_block()
         self.assertIn(f'#PBS -l select={pbs_script.num_nodes}:ncpus={NODE_TYPES["narwhal"]["compute"]}:'
                       f'mpiprocs={pbs_script.processes_per_node}', res)
 
     def test_get_render_required_directives_block_for_narwhal_bigmem_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=1, max_time="20:30:30",
-                            node_type='bigmem', system='narwhal')
+                               node_type='bigmem', system='narwhal')
         res = pbs_script.render_required_directives_block()
         self.assertIn(f'#PBS -l select={pbs_script.num_nodes}:ncpus={NODE_TYPES["narwhal"]["bigmem"]}:'
                       f'mpiprocs={pbs_script.processes_per_node}', res)
 
     def test_get_render_required_directives_block_for_narwhal_transfer_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=1, max_time="20:30:30",
-                            node_type='transfer', system='narwhal')
+                               node_type='transfer', system='narwhal')
         res = pbs_script.render_required_directives_block()
         self.assertIn('#PBS -l select=5:ncpus=1', res)
 
     def test_get_render_required_directives_block_for_carpenter_compute_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=24, max_time="20:30:30",
-                            node_type='compute', system='carpenter')
+                               node_type='compute', system='carpenter')
         expected = f'#PBS -l select={pbs_script.num_nodes}:ncpus={NODE_TYPES["carpenter"]["compute"]}:' \
                    f'mpiprocs={pbs_script.processes_per_node}'
 
@@ -187,7 +187,7 @@ class TestPBSScript(unittest.TestCase):
 
     def test_get_render_required_directives_block_for_carpenter_gpu_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=32, max_time="20:30:30",
-                            node_type='gpu', system='carpenter')
+                               node_type='gpu', system='carpenter')
         expected = f'#PBS -l select={pbs_script.num_nodes}:ncpus={NODE_TYPES["carpenter"]["gpu"]}:' \
                    f'mpiprocs={pbs_script.processes_per_node}:ngpus=1'
 
@@ -196,7 +196,7 @@ class TestPBSScript(unittest.TestCase):
 
     def test_get_render_required_directives_block_for_carpenter_bigmem_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=24, max_time="20:30:30",
-                            node_type='bigmem', system='carpenter')
+                               node_type='bigmem', system='carpenter')
         expected = f'#PBS -l select={pbs_script.num_nodes}:ncpus={NODE_TYPES["carpenter"]["bigmem"]}:' \
                    f'mpiprocs={pbs_script.processes_per_node}:bigmem=1'
 
@@ -205,7 +205,7 @@ class TestPBSScript(unittest.TestCase):
 
     def test_get_render_required_directives_block_for_carpenter_transfer_node(self):
         pbs_script = PbsScript(name='test1', project_id='P001', num_nodes=5, processes_per_node=1, max_time="20:30:30",
-                            node_type='transfer', system='carpenter')
+                               node_type='transfer', system='carpenter')
         res = pbs_script.render_required_directives_block()
         self.assertIn('PBS -l select=5:ncpus=1', res)
 
